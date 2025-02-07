b14e076d4b6cfa43b6c1513df9001ab652c4c185
tomoya <alligatorfree12@gmail.com>
2025-02-07 14:04:23 +0900

    24. Text Selector

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.describe('Home', () => {
   test('Open homepage and verify title', async ({ page }) => {
@@ -7,9 +7,21 @@ test.describe('Home', () => {
     await expect(page).toHaveTitle('Practice E-Commerce Site – SDET Unicorns');
   });
 
+  // 23. CSS Selector
   test('Click get started button button using CSS selector', async ({ page }) => {
     await page.goto('https://practice.sdetunicorns.com/');
 
     await page.locator('#get-started').click();
+
+    await expect(page).toHaveURL(/.*#get-started/);
+  });
+
+  // 24. Text Selector
+  test('verifying heading text is visible using text selector', async ({ page }) => {
+    await page.goto('https://practice.sdetunicorns.com/');
+
+    const headingText = await page.locator('text=Think different. Make different.');
+
+    await expect(headingText).toBeVisible();
   });
 });
